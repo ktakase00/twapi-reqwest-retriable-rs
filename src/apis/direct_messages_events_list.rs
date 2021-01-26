f5029ecf354314ab61ff022649955ce5b66968b0
aoyagikouhei <aoyagi_kouhei@uniquevision.co.jp>
2021-01-26 18:20:15 +0900

    modify result

--- src/apis/direct_messages_events_list.rs
+++ src/apis/direct_messages_events_list.rs
@@ -1,5 +1,4 @@
-use crate::{error::RetriableError, LogParams, Retriable, RETRIABLE_ERRORS};
-use twapi_reqwest::serde_json::Value;
+use crate::{error::RetriableError, LogParams, Retriable, RetriableResult, RETRIABLE_ERRORS};
 
 impl Retriable {
     pub async fn direct_messages_events_list(
@@ -8,10 +7,10 @@ impl Retriable {
         retry_count: usize,
         retry_delay_secound_count: usize,
         log: &impl Fn(LogParams),
-    ) -> Result<Value, RetriableError> {
+    ) -> Result<RetriableResult, RetriableError> {
         let path = "https://api.twitter.com/1.1/direct_messages/events/list.json";
         let log_params = LogParams::new(path, params);
-        Ok(self
+        self
             .execute(
                 retry_count,
                 Some(retry_delay_secound_count),
@@ -29,7 +28,6 @@ impl Retriable {
                     )
                 },
             )
-            .await?
-            .result)
+            .await
     }
 }
